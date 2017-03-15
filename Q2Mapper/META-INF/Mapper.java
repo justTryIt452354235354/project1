7bb9b81e8db9fe5735ffc975b9a9697764d417dd
longmango <alaska_lee@163.com>
2017-03-15 18:16:01 -0400

    improve the speed to 79s

---
+++ Q2Mapper/META-INF/Mapper.java
@@ -0,0 +1,229 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import org.json.simple.JSONArray;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.json.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Mapper {
+    private final static String FILENAME = "part-r-00000";
+    private static HashSet<Object> idSet = new HashSet<>();
+    private static HashSet<Object> idStrSet = new HashSet<>();
+    
+    // first step
+    public static boolean isMalformed(JSONObject obj) {
+        
+        // hashtag text (stated above) is missing or empty
+        JSONObject entities = (JSONObject) obj.get("entities");
+        if (entities == null || entities.isEmpty()) return true;
+        
+        JSONArray hashtags = (JSONArray) entities.get("hashtags");
+        if (hashtags == null || hashtags.size() == 0) return true;
+        
+        /*
+        for (int j = 0; j < hashtags.size(); j++) {
+            JSONObject o = (JSONObject) hashtags.get(j);
+            if (o.get("text") == null || o.get("text").toString().isEmpty()) return true;
+        }
+        */
+        // Both id and id_str of the tweet object are missing or empty
+        Object id = obj.get("id");
+        Object idStr = obj.get("id_str");
+        if ((id == null || id.toString().isEmpty()) && (idStr == null || idStr.toString().isEmpty())) {
+            //System.out.println("==================tid is missing");
+            return true;
+        }
+        if (id != null && !id.toString().isEmpty()) {
+            if (idSet.contains(id))
+                return true;
+            else
+                idSet.add(id);
+        }
+        if (idStr != null && !idStr.toString().isEmpty()) {
+            if (idStrSet.contains(idStr))
+                return true;
+            else
+                idStrSet.add(idStr);
+        }
+        
+        
+        // text field is missing or empty
+        Object text = obj.get("text");
+        if (text == null || text.toString().isEmpty()) {
+            //System.out.println("==================text is missing");
+            return true;
+        }
+        
+        // created_at field is missing or empty
+        Object created_at = obj.get("created_at");
+        if (created_at == null || created_at.toString().isEmpty()) {
+            //System.out.println("==================created at is missing");
+            return true;
+        }
+        
+        
+        
+        // Both id and id_str in user object are missing or empty
+        JSONObject user = (JSONObject) obj.get("user");
+        if (user == null || user.isEmpty()) return true;
+      
+        Object id2 = user.get("id");
+        Object idStr2 = user.get("id_str");
+        if ((id2 == null || id2.toString().isEmpty()) && (idStr2 == null || idStr2.toString().isEmpty())) {
+            //System.out.println("==================uid is missing" + obj.toJSONString());
+            return true;
+        }
+        
+        
+        
+        // lang field is missing or empty or malformed
+        Object lang = obj.get("lang");
+        if (lang == null || lang.toString().isEmpty()) {
+            //System.out.println("==================lang is missing");
+            return true;
+        }
+        String langValue = lang.toString();
+        final String regex = "^(ar|en|fr|in|pt|es|tr)$";
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(langValue);
+        if (!matcher.find()) {
+            return true;
+        }
+        
+        
+        
+        return false;
+    }
+
+
+    public static String isShortenedURLs(String line) {
+        final String regex = "(https?|ftp):\\/\\/[\\.[a-zA-Z0-9]\\/\\-_]+";
+        line = line.replaceAll(regex,"");
+
+        return line;
+    }
+    
+
+
+    private static void map() {
+        HashSet<String> stopwords = readStopWords();
+        BufferedReader br;
+        HashMap<String, JSONObject> result = new HashMap<String, JSONObject>();
+
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(FILENAME), StandardCharsets.UTF_8));
+            
+            PrintWriter printWriter = new PrintWriter(new File("reducer_Result_final"),"UTF-8");
+            //PrintStream printWriter = new PrintStream(System.out, true, "UTF-8");
+            String line;
+            while((line = br.readLine()) != null) {
+                try {
+                    JSONObject lineResult = new JSONObject();
+
+                    JSONParser parser = new JSONParser();
+                    line = isShortenedURLs(line.replaceAll("\\\\/", "/"));
+                    JSONObject obj = (JSONObject) parser.parse(line);//http://stackoverflow.com/questions/13939925/remove-all-occurrences-of-from-string
+
+                    if (isMalformed(obj)) {
+                        //System.out.println("===========malformed json==========");
+                        continue;
+                    }
+                    
+                    String text = obj.get("text").toString();
+                    HashMap<String, Integer> wordFreq = effectiveWord(text, stopwords);
+                    lineResult.put("text", wordFreq);
+
+                    JSONObject user = (JSONObject) obj.get("user");
+                    Object id = user.get("id");
+                    Object idStr = user.get("id_str");
+                    if (id == null || id.toString().isEmpty()) {
+                        String userid = idStr.toString();
+                        lineResult.put("userid", userid);
+                    } else {
+                        String userid = id.toString();
+                        lineResult.put("userid", userid);
+                    }
+
+                    JSONObject entities = (JSONObject) obj.get("entities");
+                    JSONArray hashtags = (JSONArray) entities.get("hashtags");
+
+                    for (Object hashtag : hashtags) {
+                        JSONObject tag = (JSONObject) hashtag;
+                        
+                        if (tag.get("text") == null || tag.get("text").toString().isEmpty()) continue;
+                        
+                        String hashTag_text = tag.get("text").toString();
+                        if (result.get(hashTag_text) == null) {
+                            result.put(hashTag_text, lineResult);
+                        } else {
+                            HashMap<String, JSONObject> tmp = new HashMap<String, JSONObject>();
+                            tmp.put(hashTag_text, lineResult);
+                            result.putAll(tmp);
+                        }
+                    }
+                } catch (ParseException e) {
+                    continue; // cannot be parsed as a JSON object
+                }
+                //printWriter.flush();
+            }
+            for (Map.Entry<String, JSONObject>  entry : result.entrySet()) {
+                printWriter.write(entry.getKey() + "\t");
+                printWriter.print(entry.getValue().toString() + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public static HashSet<String> readStopWords() {
+        String FILENAME = "stopwords.txt";
+        HashSet<String> stopwords = new HashSet<String>();
+        
+        String line = null;
+        try {
+            FileReader fileReader = new FileReader(FILENAME);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                stopwords.add(line);
+            }
+            bufferedReader.close();  
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stopwords;
+    }
+    
+    public static HashMap<String, Integer> effectiveWord(String str, HashSet<String> stopwords) {
+        HashMap<String, Integer> frequencyMap = new HashMap<String, Integer>();
+        String[] arr = str.split(" ");
+        for (String s : arr) {
+            if (!stopwords.contains(s.toLowerCase())) {
+                frequencyMap.put(s, frequencyMap.getOrDefault(s,0) + 1);
+            }
+        }
+        return frequencyMap;
+    }
+
+
+
+    public static void main(String[] args) {
+        StopWatch timer1 = new StopWatch();
+        map();
+        System.out.println(timer1.elapsedTime()/1000 + "sec");
+    }
+    
+    
+    
+    
+    
+}
