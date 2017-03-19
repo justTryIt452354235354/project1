d1de3d63907cd5056ff6f05a56a7dbf8996962a7
Jiawen Zhu <zackzhu1@gmail.com>
2017-03-19 12:06:32 -0400

    add parse logic

--- Q2_undertow/src/main/java/cc/cmu/edu/Q1/Q2MySQLServlet.java
+++ Q2_undertow/src/main/java/cc/cmu/edu/Q1/Q2MySQLServlet.java
@@ -1,6 +1,13 @@
 package cc.cmu.edu.Q1;
 
 import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -10,6 +17,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 
@@ -24,6 +36,9 @@ public class Q2MySQLServlet extends HttpServlet {
     private static final String URL = "jdbc:mysql://ccdb.c6nxguhfkaz7.us-east-1.rds.amazonaws.com/" + DB_NAME + "?useSSL=false";
     private static final String DB_USER = System.getenv("db_user"); //TODO
     private static final String DB_PWD = System.getenv("db_pwd"); //TODO
+    private static final String TEAM_ID = "";
+    private static final String TEAM_AWS_ACCOUNT_ID = "";
+    
     
     private static Connection connection;
     
@@ -45,13 +60,23 @@ public class Q2MySQLServlet extends HttpServlet {
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) 
             throws ServletException, IOException {
-    	String result = "";
-    	
     	String hashtag = request.getParameter("hashtag");
     	String N = request.getParameter("N");
     	String list_of_key_words = request.getParameter("list_of_key_words");
-    	 	
-    	result = queryFromMySQL(hashtag, N, list_of_key_words);
+    	keywordSet = getKeyWordList(list_of_key_words);
+    	
+    	String result = "";
+    	if (hashtag == null) {
+       		result = TEAM_ID + "," + TEAM_AWS_ACCOUNT_ID + "\n";
+       	} else {
+       		String column = queryFromMySQL(hashtag, N, list_of_key_words);
+        	try {
+    			String output = parse(column, Integer.parseInt(N));
+    			result = TEAM_ID + "," + TEAM_AWS_ACCOUNT_ID + "\n" + output + "\n";
+    		} catch (ParseException e) {
+    			e.printStackTrace();
+    		}
+       	}
     	
     	PrintWriter writer = new PrintWriter(
                 new OutputStreamWriter(response.getOutputStream(), "UTF8"), true);
@@ -59,9 +84,62 @@ public class Q2MySQLServlet extends HttpServlet {
         writer.close();
     }
     
+    private String parse(String input, Integer N) throws ParseException {
+    	HashMap<String, Integer> map = new HashMap<>(); // userId, sum
+    	JSONParser jsonParser = new JSONParser();
+    	Object obj = jsonParser.parse(input);
+    	JSONArray array = (JSONArray) obj;
+    	for (int i = 0; i < array.size(); i++) {
+    		JSONObject jsonObj = (JSONObject) array.get(i);
+    		JSONObject textObj = (JSONObject) jsonObj.get("text");
+    		int value = getSum(textObj);
+    		String useridObj = (String) jsonObj.get("userid");
+    		map.put(useridObj, value);
+    	}
+    	
+    	// sort
+    	Set<Entry<String, Integer>> set = map.entrySet();
+    	ArrayList<Entry<String, Integer>> list = new ArrayList<>(set);
+    	list.sort(new Comparator<Entry<String, Integer>>() {
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				int tmp = o2.getValue().compareTo(o1.getValue());
+				if (tmp == 0) {
+					tmp = o1.getKey().compareTo(o2.getKey());
+				}
+				return tmp;
+			}		
+    	});
+    	
+    	StringBuilder sb = new StringBuilder();
+    	int number = list.size() < N ? list.size() : N;
+    	for (int i = 0; i < number - 1; i++) {
+			String userId = list.get(i).getKey();
+			Integer sum = list.get(i).getValue();
+			sb.append(userId + ":" + sum + ",");
+		}
+		sb.append(list.get(list.size() - 1).getKey() + ":" + list.get(list.size() - 1).getValue());
+		
+    	return sb.toString();
+    }
+    
+    private static int getSum(JSONObject jObject) {
+    	int sum = 0;
+    	Iterator<?> keys = (Iterator<?>) jObject.keySet();
+  
+    	while (keys.hasNext()) {
+    		String key = (String) keys.next();
+    		if (keywordSet.contains(key)) {
+    			Integer value = (Integer) jObject.get(key);
+    			sum += value;  			
+    		}
+    	}
+    	return sum;
+    }
+    
     // TODO
     private String queryFromMySQL(String hashtag, String N, String list_of_key_words) throws ServletException, IOException {
-    	String result = ""; //TODO
+       	String result = ""; //TODO
     	String sql = ""; //TODO
     	PreparedStatement statement = null;
     	ResultSet resultSet = null;    	
@@ -80,6 +158,16 @@ public class Q2MySQLServlet extends HttpServlet {
         }
     	return result;
     }
+
+    
+    private static HashSet<String> keywordSet = null;
+    private static HashSet<String> getKeyWordList(String list) {
+    	HashSet<String> result = new HashSet<>();
+    	for (String str : list.split(",")) {
+    		result.add(str);
+    	}
+    	return result;
+    }
     
     @Override
     protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
@@ -87,7 +175,6 @@ public class Q2MySQLServlet extends HttpServlet {
         doGet(request, response);
     }
 }
-
 
 
 
